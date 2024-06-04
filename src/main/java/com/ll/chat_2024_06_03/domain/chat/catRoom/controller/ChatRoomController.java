ba1ba0d8603ee4f99931f25c748c911ab107d162
kim-chaeyeon <20212621@gm.hannam.ac.kr>
2024-06-04 16:20:49 +0900

    chatMessage write

--- src/main/java/com/ll/chat_2024_06_03/domain/chat/catRoom/controller/ChatRoomController.java
+++ src/main/java/com/ll/chat_2024_06_03/domain/chat/catRoom/controller/ChatRoomController.java
@@ -16,11 +16,14 @@ public class ChatRoomController {
     private final ChatRoomService chatRoomService;
 
     @GetMapping("/{roomId}")
-    @ResponseBody
     public String showRoom(
             @PathVariable("roomId") final long roomId,
-            @RequestParam(value = "writerName", defaultValue = "NoName") final String writerName
+            @RequestParam(value = "writerName", defaultValue = "NoName") final String writerName,
+            Model model
     ) {
+        ChatRoom room = chatRoomService.findById(roomId).get();
+        model.addAttribute("room", room);
+
         return "domain/chat/chatRoom/room";
     }
 
@@ -42,5 +45,16 @@ public class ChatRoomController {
         List<ChatRoom> chatRooms = chatRoomService.findAll();
         model.addAttribute("chatRooms", chatRooms);
         return "domain/chat/chatRoom/list";
+    }
+
+    @PostMapping("/{roomId}/write")
+    public String write(
+            @PathVariable("roomId") final long roomId,
+            @RequestParam(value = "writerName") final String writerName,
+            @RequestParam(value = "content") final String content
+    ) {
+        chatRoomService.write(roomId, writerName, content);
+
+        return "redirect:/chat/room/" + roomId;
     }
 }
